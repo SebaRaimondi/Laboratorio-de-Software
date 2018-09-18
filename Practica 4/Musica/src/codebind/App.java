71812d668312e2338c427acb4156890c802ced6d
Sebastian Raimondi <sebastianraimondi@gmail.com>
2018-09-18 04:22:50 -0300

    Casi terminado

--- Practica 4/Musica/src/codebind/App.java
+++ Practica 4/Musica/src/codebind/App.java
@@ -1,15 +1,27 @@
 package codebind;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableColumnModel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class App {
+    private int rows = 13;
+    private int cols = 27;
+
+    private JTable last_target;
+    private String last_note;
+
     private JButton button_msg;
     private JPanel panelMain;
     private JTextField selected_partition;
@@ -26,8 +38,10 @@ public class App {
     private JButton button6;
     private JButton button7;
     private JPanel pentagram_panel;
+    private JPanel pentagram_panel_img;
     private JTable pentagram_table;
     private String current_note = "R";
+    private String[] song = new String[cols];
 
     public App() {
         // Assign note to buttons
@@ -61,19 +75,100 @@ public class App {
                 JTable target = (JTable)e.getSource();
                 int row = target.getSelectedRow();
                 int column = target.getSelectedColumn();
-                System.out.println("pene row: "+row);
-                System.out.println("pene col: "+column);
-
                 setCellValue(target, column, row);
+            }
+        });
+        clear_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                for (int i = 0; i < cols; i++) {
+                    clearColumn(pentagram_table, i);
+                    song[i] = null;
+                    selected_partition.setText("");
+                }
+            }
+        });
+        back_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (last_target == null) return;
+                String x = current_note;
+                current_note = last_note;
+                setCellValue(last_target, last_target.getSelectedColumn(), last_target.getSelectedRow());
+                current_note = x;
             }
         });
     }
 
+    private String translateToDuration(String in) {
+        String out = "";
+        switch (in) {
+            case "R": out = "w";
+                break;
+            case "B": out = "h";
+                break;
+            case "N": out = "q";
+                break;
+            case "C": out = "i";
+                break;
+            case "SC": out = "s";
+                break;
+            case "F": out = "t";
+                break;
+            case "SF": out = "x";
+                break;
+        }
+        return out;
+    }
+
+    private String translateNote(int in) {
+        String out = "";
+        switch (in) {
+            case 1: out = "C";
+                break;
+            case 2: out = "D";
+                break;
+            case 3: out = "E";
+                break;
+            case 4: out = "F";
+                break;
+            case 5: out = "G";
+                break;
+            case 6: out = "A";
+                break;
+            case 0: out = "B";
+                break;
+        }
+        return out;
+    }
+
+    private String setSection(boolean same, int row) {
+        if (same || current_note == null) return null;
+
+        String note = translateNote(((row - rows) * -1)%7);
+        String octave = String.valueOf((((row - rows) * -1)/8)+5);
+        String duration = translateToDuration(current_note);
+        return note + octave + duration;
+    }
+
     private void setCellValue(JTable target, int column, int row) {
-        boolean same_as_selected = target.getValueAt(row, column) == current_note;
+        System.out.println("last note= "+last_note);
+        last_note = (String)target.getValueAt(row, column);
+        last_target = target;
+        System.out.println("current note= "+current_note);
+        boolean same_as_selected = target.getValueAt(row, column) == (current_note);
         String value = same_as_selected ? "" : current_note;
         clearColumn(target, column);
         target.setValueAt(value, row, column);
+
+        song[column] = setSection(same_as_selected, row);
+
+        String full_song = "";
+        for (String s : song) {
+            if (s != null) full_song += s + " ";
+        }
+
+        selected_partition.setText(full_song);
     }
 
     private void clearColumn(JTable target, int column) {
@@ -83,16 +178,6 @@ public class App {
         }
     }
 
-    /*
-        public App() {
-            button_msg.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    JOptionPane.showMessageDialog(null, "Hello World");
-                }
-            });
-        }
-    */
     public static void main(String[] args) {
         JFrame frame = new JFrame("App");
         frame.setContentPane(new App().panelMain);
@@ -103,9 +188,20 @@ public class App {
 
     private void createUIComponents() {
         // Crear tabla y asignarle la cantidad de filas y columnas
-        pentagram_panel = new JPanel();
-        int rows = 10;
-        int cols = 30;
+        pentagram_panel = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+
+                try {
+                    BufferedImage myPicture = ImageIO.read(new File("src/img/Music/pentagrama.png"));
+
+                    g.drawImage(myPicture, 0, 0, null);
+                } catch (IOException ex) {
+                    System.out.print(ex.toString());
+                }
+            }
+        };
         pentagram_table = new JTable(rows, cols){
             @Override
             public boolean isCellEditable(int row, int column) {
@@ -119,14 +215,22 @@ public class App {
         for (int i = 0; i < cols; i++) {
             columnModel.getColumn(i).setPreferredWidth(35);
         }
+        pentagram_table.setRowHeight(10);
 
         // Make table background transparent
         pentagram_table.setOpaque(false);
-        ((DefaultTableCellRenderer)pentagram_table.getDefaultRenderer(Object.class)).setOpaque(false);
+        DefaultTableCellRenderer renderer = (DefaultTableCellRenderer)pentagram_table.getDefaultRenderer(Object.class);
+        renderer.setOpaque(false);
+        renderer.setHorizontalAlignment( SwingConstants.CENTER );
 
         // Remove table grid lines
-        // pentagram_table.setShowGrid(false);
+        pentagram_table.setShowGrid(false);
 
+        pentagram_table.setFont(new Font("Arial", Font.PLAIN, 12));
+
+        EmptyBorder border = new EmptyBorder(26, 100, 26,0);
+        pentagram_panel.setBorder(border);
+        pentagram_panel.setPreferredSize(new Dimension(1070,202));
         pentagram_panel.add(pentagram_table);
     }
 }
